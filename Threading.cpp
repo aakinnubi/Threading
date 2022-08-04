56bb227a9391a6d0331d415d4157d8a3036315a5
Abiola Akinnubi <abiola.akinnubi@activision.com>
2022-08-04 11:52:09 -0500

    Added Shards for splitting data

--- Threading.cpp
+++ Threading.cpp
@@ -7,7 +7,6 @@
 #include "Threading.h"
 int counts = 0;
 int maxSizePerShard = 50;
-void ShardUpdates(std::vector<Blog>& blogs, const size_t& partitions, vector<Shard>& vectorShared);
 void CounterThread()
 {
 
@@ -20,6 +19,27 @@ void CounterThread()
     return;
  
 }
+void ShardUpdates(std::vector<Blog>& blogs, const size_t & partitions, vector<Shard>& vectorShared, const int &index)
+{ 
+
+    if (index == 0) {
+        std::vector<Blog> half1(blogs.begin(), blogs.begin() + partitions);
+        vectorShared.push_back(Shard(1, half1));
+    }
+    else {
+        std::vector<Blog> half2(blogs.begin() + partitions, blogs.end());
+        vectorShared.push_back(Shard(2, half2));
+    }
+
+}
+//void ShardUpdates(std::vector<Blog>& blogs, const size_t& partitions, vector<Shard>& vectorShared)
+//{
+//    std::vector<Blog> half1(blogs.begin(), blogs.begin() + partitions);
+//    vectorShared.push_back(Shard(1, half1));
+//    std::vector<Blog> half2(blogs.begin() + partitions, blogs.end());
+//    vectorShared.push_back(Shard(2, half2));
+//
+//}
 void AddBulkItems() {
     string title = "Test blog post ";
     string content = "Test blog content ";
@@ -27,15 +47,21 @@ void AddBulkItems() {
     int count = 100;
    vector<Blog> blogs;
     for (int index = 0; index <= count; ++index) {
-        title = title + std::to_string(index);
-        content = content + std::to_string(index);
+        title = title ;
+        content = content;
         blogs.push_back(Blog(index, title, content));
     }
  
-    std::size_t const partitions = blogs.size() / maxSizePerShard;
+    std::size_t const partitions = maxSizePerShard;
+        //blogs.size() / 2;
     vector<Shard> vectorShared;
-    ShardUpdates(blogs, partitions, vectorShared);
-
+    std::thread SharedJoined1(ShardUpdates, std::ref(blogs), std::ref(partitions), std::ref(vectorShared), (0));
+    SharedJoined1.join();
+    std::thread SharedJoined2(ShardUpdates, std::ref(blogs), std::ref(partitions), std::ref(vectorShared), (1));
+    SharedJoined2.join();
+  /*  std::thread SharedJoined();
+    SharedJoined.join();*/
+ /*   ShardUpdates(blogs, partitions, vectorShared);*/
     for (Shard _shard: vectorShared) {
         string message = "Currently on Shard " + to_string(_shard.GetShardId());
         string messageEnd = "Done with Shard " + to_string(_shard.GetShardId());
@@ -46,16 +72,12 @@ void AddBulkItems() {
 
         cout << messageEnd << endl;
     }
+    
+    
+  /*  SharedJoined.join();*/
 }
 
-void ShardUpdates(std::vector<Blog>& blogs, const size_t& partitions, vector<Shard>& vectorShared)
-{
-    std::vector<Blog> half1(blogs.begin(), blogs.begin() + partitions);
-    std::vector<Blog> half2(blogs.begin() + partitions, blogs.end());
-  
-    vectorShared.push_back(Shard(1, half1));
-    vectorShared.push_back(Shard(1, half2));
-}
+
 
 int main()
 {
